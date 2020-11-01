58c1400c6efbcb39c1653677cae54be59e25a8e3
Ilya Novikov <groyn29@gmail.com>
2020-11-01 19:25:19 +0300

    Config vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,59 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
+const URL = "https://5daca7380af117001417072a.mockapi.io";
+
 export default new Vuex.Store({
   state: {
+    courses: [],
     lessons: [],
-    selectedLesson: null
+    loading: false,
+    selectedCourseId: null
   },
   mutations: {
+    setCourses(state, courses) {
+      state.courses = courses;
+    },
+
     setLessons(state, lessons) {
       state.lessons = lessons;
+    },
+
+    setLessonId(state, lessonId) {
+      state.selectedCourseId = lessonId;
+    },
+
+    setLoading(state, loading) {
+      state.loading = loading;
     }
   },
   actions: {
-    getLessons({ commit }) {
-      commit("setLessons", []);
+    async getCourses({ commit }) {
+      const request = await axios.get(`${URL}/api/v1/courses`);
+      commit("setCourses", request.data);
+    },
+
+    async getLessons({ commit }) {
+      const request = await axios.get(`${URL}/api/v1/lessons`);
+      commit("setLessons", request.data);
     }
   },
+  getters: {
+    courses: state => {
+      return state.courses;
+    },
+
+    selectedLessonsByCourseId: state => {
+      return state.lessons.filter(el => el.courseId === state.selectedCourseId);
+    },
+
+    loading: state => {
+      return state.loading;
+    }
+  },
+
   modules: {}
 });
